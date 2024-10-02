e23df8db33802b5fd5c160f992b947223621b88d
slayerjk <slayer.jk@mail.ru>
2024-10-02 11:44:18 +0500

    initial code

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -6,15 +6,15 @@ import (
 	"os"
 )
 
-// Start logging via log package
-func StartLogging(logName, logDirPath string, logsToKeep int) (*os.File, error) {
+// Start logging via log package; creates log file as <fileName>_<mm.dd.YYYY>.log
+func StartLogging(fileName, logDirPath string, logsToKeep int) (*os.File, error) {
 	// create log dir
 	if err := os.MkdirAll(logDirPath, os.ModePerm); err != nil {
 		return nil, fmt.Errorf("failed to create log dir %s:\n\t%v", logDirPath, err)
 	}
 
 	// create log file
-	logFilePath := fmt.Sprintf("%s/%s.log", logDirPath, logName)
+	logFilePath := fmt.Sprintf("%s/%s.log", logDirPath, fileName)
 
 	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
