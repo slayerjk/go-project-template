79f67316fa354a60b8526d2aac44ff4847cd1168
slayerjk <slayer.jk@mail.ru>
2024-10-28 14:42:05 +0500

    *main refactor(exe path)

--- cmd/myapp/myapp.go
+++ cmd/myapp/myapp.go
@@ -3,25 +3,37 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"path/filepath"
 	"time"
 
 	// change this path for your project
-	"template/internal/logging"
-	"template/internal/rotatefiles"
-	// "template/internal/mailing"
+	"github.com/slayerjk/go-project-template/internal/logging"
+	"github.com/slayerjk/go-project-template/internal/rotatefiles"
+	// "github.com/slayerjk/go-project-template/internal/mailing"
 )
 
 // log default path & logs to keep after rotation
 const (
-	appName           = "MYAPP"
-	defaultLogPath    = "logs"
-	defaultLogsToKeep = 3
-	// mailingFile       = "data/mailing.json"
+	appName = "MYAPP"
 )
 
-var startTime = time.Now()
-
 func main() {
+	// get executable's working dir
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	exePath := filepath.Dir(exe)
+
+	// defining default values
+	var (
+		startTime         time.Time = time.Now()
+		defaultLogPath    string    = exePath + "/logs"
+		defaultLogsToKeep int       = 3
+		// mailingFile       string = exePath + "/data/mailing.json"
+	)
+
 	// flags
 	logDir := flag.String("log-dir", defaultLogPath, "set custom log dir")
 	logsToKeep := flag.Int("keep-logs", defaultLogsToKeep, "set number of logs to keep after rotation")
@@ -35,7 +47,7 @@ func main() {
 	defer logFile.Close()
 
 	// starting programm notification
-	log.Println("Program Started")
+	log.Printf("%s Started", appName)
 
 	// main code here
 
